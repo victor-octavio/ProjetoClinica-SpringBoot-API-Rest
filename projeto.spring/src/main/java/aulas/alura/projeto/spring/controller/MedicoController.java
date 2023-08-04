8bddc794fd75905ecf54ef3f0682aab4d1183e8b
Victor <victoroctavio4@gmail.com>
2023-08-04 14:48:30 -0300

    Autenticação de usuário e controle de acesso

--- projeto.spring/src/main/java/aulas/alura/projeto/spring/controller/MedicoController.java
+++ projeto.spring/src/main/java/aulas/alura/projeto/spring/controller/MedicoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-@RequestMapping("medicos")
+@RequestMapping("/medicos")
 
 public class MedicoController {
 
